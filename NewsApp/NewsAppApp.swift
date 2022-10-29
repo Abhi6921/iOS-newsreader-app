cd226edf3edf26c4aae8b0e98640da01f2ff0c9f
Abhishek <abhishek.narvekar80@gmail.com>
2022-10-29 12:04:24 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Abhishek Narvekar on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
